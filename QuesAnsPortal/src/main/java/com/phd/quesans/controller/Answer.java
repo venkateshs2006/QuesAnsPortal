eef10efcaa4b8416c1cf33ea6219cc41c1e8cae9
SV <SV@SV-PC>
2015-11-08 23:46:29 +0530

    8thnov commit

--- QuesAnsPortal/src/main/java/com/phd/quesans/controller/Answer.java
+++ QuesAnsPortal/src/main/java/com/phd/quesans/controller/Answer.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.google.gson.Gson;
 import com.phd.quesans.Crawler.WebpageCrawer;
 import com.phd.quesans.Service.QuesAnsService;
 import com.phd.quesans.Service.SearchEngineService;
@@ -23,59 +24,57 @@ import com.phd.quesans.pojo.SearchEngine;
 
 public class Answer {
 	@Autowired
-	QuesAnsService quesAnsService;	
+	QuesAnsService quesAnsService;
+
 	@RequestMapping(value = "/RequestQuestion", method = RequestMethod.POST)
-    public String processQuestionRequest(@RequestParam("ques") String name,
-            Map<String, Object> model) {         
-        // implement your own registration logic here...         
-        // for testing purpose:
-		QuestionPojo questionPojo=getQuestionPojo(name);
+	public String processQuestionRequest(@RequestParam("ques") String name, Map<String, Object> model) {
+		// implement your own registration logic here...
+		// for testing purpose:
+		QuestionPojo questionPojo = getQuestionPojo(name);
 		model.put("ques", questionPojo.getQuestion());
-        model.put("answer", questionPojo.getAnswer());
-        List<String> resultWebContent=getWikiContent(questionPojo.getQuestion(),questionPojo.getKeywords());
-        model.put("wiki", resultWebContent.get(0));
-        model.put("google", resultWebContent.get(1));
-        return "QuestionSuccess";
-    }
+		model.put("answer", questionPojo.getAnswer());
+		List<String> resultWebContent = getWikiContent(questionPojo.getQuestion(), questionPojo.getKeywords());
+		model.put("wiki", resultWebContent.get(0));
+		model.put("google", resultWebContent.get(1));
+		return "QuestionSuccess";
+	}
+
 	@RequestMapping(value = "/RequestQuestion", method = RequestMethod.GET)
-    public String processQuestionGetRequest(@RequestParam("ques") String name,
-            Map<String, Object> model) {         
-        // implement your own registration logic here...         
-        // for testing purpose:
-		
-		QuestionPojo questionPojo=getQuestionPojo(name);
+	public String processQuestionGetRequest(@RequestParam("ques") String name, Map<String, Object> model) {
+		// implement your own registration logic here...
+		// for testing purpose:
+
+		QuestionPojo questionPojo = getQuestionPojo(name);
 		model.put("ques", questionPojo.getQuestion());
-        model.put("answer", questionPojo.getAnswer());
-        List<String> resultWebContent=getWikiContent(questionPojo.getQuestion(),questionPojo.getKeywords());
-        model.put("wiki", resultWebContent.get(0));
-        model.put("google", resultWebContent.get(1));
-        return "QuestionSuccess";
-    }
+		model.put("answer", questionPojo.getAnswer());
+		List<String> resultWebContent = getWikiContent(questionPojo.getQuestion(), questionPojo.getKeywords());
+		model.put("wiki", resultWebContent.get(0));
+		model.put("google", resultWebContent.get(1));
+		return "QuestionSuccess";
+	}
+
 	public QuestionPojo getQuestionPojo(String question) {
 		return quesAnsService.getQuestionPojo(question);
 	}
+
 	public List<SearchEnginePojo> getSearchEnginePojo() {
 		return quesAnsService.listSearchEngine();
 	}
-   public List<String> getWikiContent(String question,String keyword) {
-	   List<String> result=new ArrayList<String>();
-	   WebpageCrawer webpageCrawer=new WebpageCrawer();
-	   List<SearchEnginePojo> searchEnginePojos=getSearchEnginePojo();
-	   for(SearchEnginePojo searchEnginePojo: searchEnginePojos){
-		  if(searchEnginePojo.getSearchEngineId()==1){
-			  System.out.println("IF condition :"+searchEnginePojo.getSearchEngineURL()+keyword+"::::"+ searchEnginePojo.getResultTag()+":::::"+ searchEnginePojo.getTagPosition());
-		   result.add(webpageCrawer.getSelectedContent(searchEnginePojo.getSearchEngineURL()+keyword, searchEnginePojo.getResultTag(), searchEnginePojo.getTagPosition()));	  
-		  }
-		  else if(searchEnginePojo.getSearchEngineId()==2){
-			  System.out.println("ELSE IF condition :"+searchEnginePojo.getSearchEngineURL()+question+"::::"+ searchEnginePojo.getResultTag()+":::::"+ searchEnginePojo.getResultTagID());
-			  result.add(webpageCrawer.getSelectedContent(searchEnginePojo.getSearchEngineURL()+question,searchEnginePojo.getResultTag(), searchEnginePojo.getResultTagID()));
-		  }
-		  else{
-			  System.out.println("ELSE condition :"+searchEnginePojo.getSearchEngineURL()+keyword+"::::"+ searchEnginePojo.getResultTag()+":::::"+ searchEnginePojo.getResultTagID());
-			  result.add(webpageCrawer.getSelectedContent(searchEnginePojo.getSearchEngineURL()+keyword,searchEnginePojo.getResultTag(), searchEnginePojo.getResultTagID()));			
-		  }
-	   }
-	   System.out.println("Result ::"+result.toString());
-	return result;  
-}
+
+	public List<String> getWikiContent(String question, String keyword) {
+		List<String> result = new ArrayList<String>();
+		WebpageCrawer webpageCrawer = new WebpageCrawer();
+		List<SearchEnginePojo> searchEnginePojos = getSearchEnginePojo();
+		for (SearchEnginePojo searchEnginePojo : searchEnginePojos) {
+			//System.out.println(https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=founder%20of%20whatsup&userip=117.202.151.21);
+			if(searchEnginePojo.getSearchEngineName()=="Wikipedia"){
+				
+			}
+			if(searchEnginePojo.equals("Google")){
+				Gson gson=new Gson();
+			}
+		}
+		System.out.println("Result ::" + result.toString());
+		return result;
+	}
 }
